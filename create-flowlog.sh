863c5600cb555fc1215e451dc311c4aebf029ecc
Thomas Ricci <thomaser87@gmail.com>
2025-06-02 14:36:31 -0700

    Update create-flowlog.sh
    fix

--- create-flowlog.sh
+++ create-flowlog.sh
@@ -5,5 +5,5 @@ echo "Subscription: $subscription - Region: $location";
 az account set -s "$subscription";
 for vnet in $(az network vnet list --query "[?location=='$location']" -o json | jq -r .[].id); do
   echo "Creating flow-logs for vNet: $vnet";
-  az network watcher flow-log create --location $location --resource-group 'vnetflowlogs-$location' --name "FL-$(basename $vnet)_" --vnet "$vnet"  --storage-account $storage_account;
+  az network watcher flow-log create --location $location --resource-group "vnetflowlogs-$location" --name "FL-$(basename $vnet)_" --vnet "$vnet"  --storage-account $storage_account;
 done;
