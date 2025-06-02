71ea83446045b840ce9cbe9b2e4aaeb7d5488540
Thomas Ricci <thomaser87@gmail.com>
2025-06-02 14:13:24 -0700

    Update create-flowlog.sh
    param storage

--- create-flowlog.sh
+++ create-flowlog.sh
@@ -1,8 +1,9 @@
 location=${1:-""} 
 subscription=${2:-""}
+storage_account=${3:-""}
 echo "Subscription: $subscription - Region: $location";
 az account set -s "$subscription";
 for vnet in $(az network vnet list --query "[?location=='$location']" -o json | jq -r .[].id); do
   echo "Creating flow-logs for vNet: $vnet";
-  az network watcher flow-log create --location $location --resource-group 'vnetflowlogs-$location' --name "FL-$(basename $vnet)_" --vnet "$vnet"  --storage-account '/subscriptions/9bd7ff15-396a-4478-a89b-4d8ae7e302b6/resourceGroups/vnetflowlogs-westus2/providers/Microsoft.Storage/storageAccounts/flstrgzh6vouaebr4qe';
+  az network watcher flow-log create --location $location --resource-group 'vnetflowlogs-$location' --name "FL-$(basename $vnet)_" --vnet "$vnet"  --storage-account $storage_account;
 done;
